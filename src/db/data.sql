c22c3cf2e87b8c6acccc7fcd9e89da7b8cc1fac3
Pienskoi <vladymyrpenskoy@gmail.com>
2020-12-25 15:56:13 +0200

    Update database

--- src/db/data.sql
+++ src/db/data.sql
@@ -21,3 +21,27 @@ UPDATE Users set room_id = '72f53380-6678-4b98' WHERE id = 2;
 UPDATE Users set room_id = '72f53380-6678-4b98' WHERE id = 3;
 
 UPDATE Users set room_id = '72f53380-6678-4b98' WHERE id = 4;
+
+INSERT INTO Shapes (room_id, path, thickness, dotted, filled) VALUES('fdf5d1d3-2d7f-46ef',
+                                                                     path('(0, 0), (15, 45), (16, 45)'), 1, FALSE, NULL);
+
+INSERT INTO Shapes (room_id, path, thickness, dotted, filled) VALUES('72f53380-6678-4b98',
+                                                                     path('(0, 0), (15, 23), (12, 56)'), 1, FALSE, NULL);
+
+INSERT INTO Shapes (room_id, path, thickness, dotted, filled) VALUES('72f53380-6678-4b98',
+                                                                     path('(0, 0), (15, 76), (45, 22)'), 1, FALSE, NULL);
+
+INSERT INTO Messages (room_id, sender_id, time, text) VALUES('72f53380-6678-4b98', 1, '04:05:06', 'test_text_11');
+
+INSERT INTO Messages (room_id, sender_id, time, text) VALUES('72f53380-6678-4b98', 5, '04:05:10', 'test_text_12');
+
+INSERT INTO Messages (room_id, sender_id, time, text) VALUES('72f53380-6678-4b98', 5, '00:01:02', 'test_text_13');
+
+INSERT INTO Messages (room_id, sender_id, time, text)
+VALUES('fdf5d1d3-2d7f-46ef', 2, '05:37:05', 'test_text_21');
+
+INSERT INTO Messages (room_id, sender_id, time, text)
+VALUES('fdf5d1d3-2d7f-46ef', 3, '05:44:09', 'test_text_22');
+
+INSERT INTO Messages (room_id, sender_id, time, text)
+VALUES('fdf5d1d3-2d7f-46ef', 4, '06:01:02', 'test_text_23');
