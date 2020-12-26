c2c3db4e64a8520d6c8d73f5cc74cc9374a02939
Pienskoi <vladymyrpenskoy@gmail.com>
2020-12-26 20:56:03 +0200

    Add EntityNotExistsException

---
+++ src/main/java/com/invicto/exceptions/EntityNotExistsException.java
@@ -0,0 +1,30 @@
+package com.invicto.exceptions;
+
+import com.invicto.domain.Room;
+import com.invicto.domain.User;
+
+public class EntityNotExistsException extends IllegalArgumentException {
+
+    public final String entityType;
+
+    EntityNotExistsException(String entityType, String entityId) {
+        super(entityType + " doesn't exist: " + entityId);
+        this.entityType = entityType;
+    }
+
+    public static EntityNotExistsException roomIsNotExist(String roomId) {
+        return new EntityNotExistsException("Room", roomId);
+    }
+
+    public static EntityNotExistsException userIsNotExist(int userId) {
+        return new EntityNotExistsException("User", String.valueOf(userId));
+    }
+
+    public static EntityNotExistsException roomIsNotExist(Room room) {
+        return roomIsNotExist(room.getId());
+    }
+
+    public static EntityNotExistsException userIsNotExist(User user) {
+        return userIsNotExist(user.getId());
+    }
+}
