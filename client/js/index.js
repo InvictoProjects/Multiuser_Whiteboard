0ad86728cd89e456b1a73007dd1183c63a485d8d
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-01-09 02:29:46 +0200

    Update js

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,15 @@
-'use strict'
+'use strict';
 
-const Http = new XMLHttpRequest();
+function createRoom() {
+    fetch('create').then(response => {
+        response.text().then(text => {
+            window.location.href += text;
+        })
+    });
+}
+
+function joinRoom() {
+    const roomId = document.getElementById('roomId').value;
+    window.location.href += roomId;
+}
+t();
