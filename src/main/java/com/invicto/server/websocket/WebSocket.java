5e8ac86991711cb7676a832fdd55a33f164d7f9f
Pienskoi <vladymyrpenskoy@gmail.com>
2021-01-10 14:56:18 +0200

    Update WebSocket

--- src/main/java/com/invicto/server/websocket/WebSocket.java
+++ src/main/java/com/invicto/server/websocket/WebSocket.java
@@ -16,8 +16,10 @@ public class WebSocket extends Thread {
     private final Logger logger = Logger.getLogger(WebSocket.class.getName());
     private Socket connection;
     private WsEventListener wsEventListener;
+    private byte[] data;
+    private boolean isText;
 
-    public final WebSocket accept(Socket connection, String request) throws IOException, NoSuchAlgorithmException {
+    public final void accept(Socket connection, String request) throws IOException, NoSuchAlgorithmException {
         this.connection = connection;
         String code="";
         String[] headers = request.split("\n");
@@ -32,23 +34,23 @@ public class WebSocket extends Thread {
         write("Connection: Upgrade");
         write("Sec-WebSocket-Accept: " + base64Enc(sha1(code + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")));
         write("");
-        return this;
     }
 
+    @Override
     public void run(){
         try {
             onOpen();
             while (true){
                 onMessage(receive());
             }
-        } catch (Exception e) {
+        } catch (WebSocketException | IOException e) {
             logger.info("WebSocket closed: " + e);
             onClose();
         } finally {
             try {
                 connection.close();
-            } catch (Exception ex) {
-                logger.log(Level.SEVERE, "WebsocketException on close:", ex);
+            } catch (IOException ioException) {
+                logger.log(Level.SEVERE, "IOException on close:", ioException);
             }
         }
     }
@@ -70,16 +72,16 @@ public class WebSocket extends Thread {
     }
 
     public void send(int opcode, byte[] data) throws IOException {
-        connection.getOutputStream().write((byte)(128+opcode));
-        if(data.length>65535){
+        connection.getOutputStream().write((byte)(128 + opcode));
+        if (data.length > 65535){
             throw new IOException();
-        }else if(data.length>125){
-            connection.getOutputStream().write((byte)126);
-            byte a = (byte)(data.length>>8);
-            byte b = (byte)(data.length%256);
+        } else if (data.length > 125){
+            connection.getOutputStream().write((byte) 126);
+            byte a = (byte)(data.length >> 8);
+            byte b = (byte)(data.length % 256);
             connection.getOutputStream().write(a);
             connection.getOutputStream().write(b);
-        }else {
+        } else {
             connection.getOutputStream().write((byte) data.length);
         }
         connection.getOutputStream().write(data);
@@ -87,79 +89,69 @@ public class WebSocket extends Thread {
     }
 
     public String receive() throws WebSocketException, IOException {
-        byte[] data;
-        boolean istext = true;
+        isText = true;
         do {
-            //header
-            byte[] header = new byte[2];
-            int end = connection.getInputStream().read(header, 0, 2);
-            int opcode = header[0] & 0x0F;
-            if (opcode == 8 || end == -1) {
-                throw new WebSocketException("Connection closed");
-            }
-            if (opcode != 1)
-                istext = false;
-
-            //datasize
-            int datasize = header[1] & 0x7F;
-            if (datasize == 126) {
-                byte[] extdatasize = new byte[2];
-                connection.getInputStream().read(extdatasize, 0, 2);
-                datasize = (extdatasize[0] << 8) + (extdatasize[1] & 0xFF);
-            } else if (datasize == 127) {
-                byte[] extdatasize = new byte[8];
-                connection.getInputStream().read(extdatasize, 0, 2);
-                if (extdatasize[0] != 0 || extdatasize[1] != 0 || extdatasize[2] != 0 ||
-                        extdatasize[3] != 0 || (extdatasize[4] & 0x80) != 0)
-                    throw new WebSocketException("recv too big data-frame");
-                datasize = (extdatasize[4] << 24) + (extdatasize[5] << 16) +
-                        (extdatasize[6] << 8) + (extdatasize[7] & 0xFF);
-            }
-
-            //mask
-            boolean ismasked = (header[1] & 0x80) != 0;
-            byte[] mask = null;
-            if (ismasked) {
-                mask = new byte[4];
-                connection.getInputStream().read(mask, 0, 4);
-            }
-
-            //data + unmask
-            data = new byte[datasize];
-            connection.getInputStream().read(data, 0, datasize);
-            if (ismasked) {
-                for (int i = 0; i < datasize; i++)
-                    data[i] = (byte) (data[i] ^ mask[i % 4]);
-            }
-            if (opcode == 0x9) {
-                send(0xA, data);
-            }
-            if (opcode != 0x1) {
-                String noTextMsg = "No-Text-msg: opcode: " + opcode + "; data: " + utf8ToString(data);
-                logger.info(noTextMsg);
-            }
-            if (opcode == 0x0) {
-                throw new WebSocketException("Continuation frames aren't supported :" + utf8ToString(data) + ".");
-            }
-        } while (!istext);
+            parseFrame();
+        } while (!isText);
         return utf8ToString(data);
     }
 
-    public String read() throws IOException {
-        StringBuilder ret = new StringBuilder();
-        while (true) {
-            byte[] proret = new byte[1];
-            int end = connection.getInputStream().read(proret,0,1);
-            if(end == -1 || proret[0] == 10)break;
-            if(proret[0] != 13)
-                ret.append((char) proret[0]);
+    private void parseFrame() throws WebSocketException, IOException {
+        //header
+        byte[] header = new byte[2];
+        int end = connection.getInputStream().read(header, 0, 2);
+        int opcode = header[0] & 0x0F;
+        if (opcode == 8 || end == -1) {
+            throw new WebSocketException("Connection closed");
         }
-        return ret.toString();
+        if (opcode != 1)
+            isText = false;
+
+        //dataSize
+        int dataSize = header[1] & 0x7F;
+        if (dataSize == 126) {
+            byte[] extDataSize = new byte[2];
+            connection.getInputStream().read(extDataSize, 0, 2);
+            dataSize = (extDataSize[0] << 8) + (extDataSize[1] & 0xFF);
+        } else if (dataSize == 127) {
+            byte[] extDataSize = new byte[8];
+            connection.getInputStream().read(extDataSize, 0, 2);
+            if (extDataSize[0] != 0 || extDataSize[1] != 0 || extDataSize[2] != 0 ||
+                    extDataSize[3] != 0 || (extDataSize[4] & 0x80) != 0)
+                throw new WebSocketException("recv too big data-frame");
+            dataSize = (extDataSize[4] << 24) + (extDataSize[5] << 16) +
+                    (extDataSize[6] << 8) + (extDataSize[7] & 0xFF);
+        }
+
+        //mask
+        boolean isMasked = (header[1] & 0x80) != 0;
+        byte[] mask = null;
+        if (isMasked) {
+            mask = new byte[4];
+            connection.getInputStream().read(mask, 0, 4);
+        }
+
+        //data + unmask
+        data = new byte[dataSize];
+        connection.getInputStream().read(data, 0, dataSize);
+        if (isMasked) {
+            for (int i = 0; i < dataSize; i++)
+                data[i] = (byte) (data[i] ^ mask[i % 4]);
+        }
+        if (opcode == 0x9) {
+            send(0xA, data);
+        }
+        if (opcode != 0x1) {
+            String noTextMsg = "No Text message: opcode: " + opcode + "; data: " + utf8ToString(data);
+            logger.info(noTextMsg);
+        }
+        if (opcode == 0x0) {
+            throw new WebSocketException("Continuation frames aren't supported :" + utf8ToString(data) + ".");
+        }
     }
 
     public void write(String msg) throws IOException {
-        msg += "\n";
-        connection.getOutputStream().write(msg.getBytes());
+        connection.getOutputStream().write((msg + "\n").getBytes());
         connection.getOutputStream().flush();
     }
 
